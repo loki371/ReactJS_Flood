2b781f3f9c5fb5351aa5df8f59989f2f7bb76976
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-05-16 16:39:41 +0700

    change Function -> React.Component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logout from "../logout/Logout"
 
-export default function Dashboard() {
-  return(
-    <div>
-      <h2>Dashboard: </h2>
-      <button type="button" onClick={Logout}>Logout!</button>
-    </div>
-  );
+class Dashboard extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>Dashboard: </h2>
+        <button type="button" onClick={Logout}>Logout!</button>
+      </div>
+    );
+  }
 }
+
+export default Dashboard;
